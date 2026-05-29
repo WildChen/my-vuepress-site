798b96aada589d3335b22e8833c58f704a7b6345
chenwei6 <chenwei6@mgtv.com>
2026-05-29 09:05:19 +0800

    chore: replace site logo with Hatsune Miku figure image
    - Downloaded and cropped original 600x800 image to 600x600 square
    - Generated 3 sizes:
      - favicon.png (32x32) for browser tab
      - logo.png (128x128) for navbar
      - apple-touch-icon.png (180x180) for iOS home screen
    - Updated config.ts head to use favicon.png and apple-touch-icon.png

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -13,7 +13,8 @@ export default defineUserConfig({
 
   head: [
     ["link", { rel: "stylesheet", href: "//at.alicdn.com/t/font_3180624_7cy10l7jqqh.css" }],
-    ["link", { rel: "icon", href: "/logo.png" }],
+    ["link", { rel: "icon", type: "image/png", href: "/favicon.png" }],
+    ["link", { rel: "apple-touch-icon", href: "/apple-touch-icon.png" }],
     ["script", { src: "/auth.js", defer: true }],
     ["script", { src: "/comments.js", defer: true }],
   ],
